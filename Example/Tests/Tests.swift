974a4de784ba1294a1ed6adc576c63dec5591a7a
Pavel Pantus <pantusp@gmail.com>
2016-10-30 10:11:09 -0400

    Attempt to fix xcode 8 TEST_HOST error

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -41,6 +41,7 @@ class ViewsSpec: QuickSpec {
             it("regular height displayed correctly") {
                 self.tester().tapView(withAccessibilityLabel: "default-3-options")
                 self.tester().waitForView(withAccessibilityLabel: "assets-action-view")
+                self.tester().acknowledgeSystemAlert()
                 expect(window).to( haveValidSnapshot() )
             }
 
